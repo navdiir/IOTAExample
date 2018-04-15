5f902ab8793868fb0398f6664e8cc8976548af58
Christopher Aldave <33591237+chrislaive@users.noreply.github.com>
2018-04-14 19:39:00 -0500

    language fix

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 const IOTA = require('iota.lib.js')
 const iota = new IOTA({ 'provider': 'http://node02.iotatoken.nl:14265' })
 
-//Creamos nuestra address, esta es inventada y no tiene importancia al ser una address para demo y de valor 0
+//Create a new address and doesnt matter which one we choose because this is a 0 value transaction
 const trytes = 'NIITLOVESIOTANIITLOVESIOTANIITLOVESIOTANIITLOVESIOTANIITLOVESIOTA999CHRISLAIVE999'
 
-//Hacemos uso de la libreria de IOTA con el método 'toTrytes()' para convertir nuestro mensaje a trytes
-let message = iota.utils.toTrytes('¡Mi primera transacción de 0 IOTAS junto al NiiT UNMSM y @Chrislaive!')
+//We used a method from iota.utils called "toTrytes()" to convert our message to Trytes
+let message = iota.utils.toTrytes('¡My first transaction to 0 IOTAS with NiiT UNMSM and @Chrislaive!')
 
-//Creamos un objeto transferencia con los valores anteriores y asignamos 'value': cero, al ser una transacción sin valor
+//We create a transfer object with that data and set value in 0 for our free transaction
 const transfers = [
     {
         value: 0,
@@ -19,17 +19,17 @@ const transfers = [
     }
 ]
 
-//Realizamos la transferencia con el método asyncrono con el callback
+//In this part we send our object transfer with a callback and give some details
 iota.api.sendTransfer(trytes, 3, 14, transfers, (error, success) => {
     if (error) {
         console.log(error)
     } else {
-        console.log('\n ¡Se realizó la transacción con exito!')
-		console.log('\n Detalle de la transacción: \n') 
+        console.log('\n Transaction Success!!')
+		console.log('\n Details of the transaction: \n') 
 		console.log(success)
-		console.log('\n\n Mensaje encriptado:\n')
+		console.log('\n\n Encrypted message:\n')
 		console.log(iota.utils.fromTrytes(message))
-		console.log('\n Ingresar a https://thetangle.org y copiar el hash en el buscador: \n')
+		console.log('\n Go to https://thetangle.org and paste this Hash for find your transaction in tangle: \n')
 		console.log(success[0].hash)
     }
 })
